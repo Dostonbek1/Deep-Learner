031b4a4688442cfef89a90a9402a9badd79ca526
lomiag <lomiag@bera.edu>
2019-03-02 15:14:14 -0500

    First_Push

---
+++ backend nn.py
@@ -0,0 +1,35 @@
+import keras
+import copy
+from pathlib import *
+mnist=keras.datasets.mnist
+import pandas as pd
+
+def make_dummy(data):
+    s = pd.Series(list('abca'))
+    pd.get_dummies(s)
+
+def n_network(data):
+
+    x_train=keras.utils.normalize(data,1)
+    y_train = keras.utils.to_categorical(data[0], 10)
+
+
+    model=keras.models.Sequential()
+    model.add(keras.layers.Dense(250,activation="relu",input_shape=len(x_train)))
+    model.add(keras.layers.Dense(250,activation="relu"))
+    model.add(keras.layers.Dense(10,keras.activations.softmax))
+
+
+    model.compile(optimizer=keras.optimizers.Adam(),
+                  loss="categorical_crossentropy",
+                  metrics=["accuracy"],
+                  )
+
+    model.fit(x_train,y_train,128,5,validation_split=0.2)
+    return model
+
+def save(model):
+    model_str=model.to_json()
+    f_path=Path("model_str.json")
+    f_path.write_text(model_str)
+    model.save_weights("model_weights.h5")
---
+++ the_recepie_script.R
@@ -0,0 +1,14 @@
+library(recipes)
+
+
+recep<-function(data_set){
+
+recepie_obj <- recipe(data_set[0] ~ ., data=data_set)%>%
+  step_dummy(all_predictors(),-all_outcomes())%>%
+  step_knnimpute(all_predictors(),-all_outcomes())%>%
+  step_center(all_predictors(),-all_outcomes())%>%
+  step_scale(all_predictors(),-all_outcomes())%>%
+  prep(data=train_tbl)
+x_train<-bake(recepie_obj, new_data=train_tbl)
+
+}
