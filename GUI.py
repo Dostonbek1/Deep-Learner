9bdd1f9b59cfc7cababe5143401ec7933a8b7972
Dostonbek <toirovd@berea.edu>
2019-03-03 09:10:42 -0500

    Complete working app

--- GUI.py
+++ GUI.py
@@ -10,6 +10,8 @@ import tkinter
 import csv
 from PIL import Image, ImageTk
 from backend_nn import *
+import rpy2.robjects as robjects
+import threading
 
 
 class Datactive:
@@ -20,85 +22,251 @@ class Datactive:
         self.path = ''
         self.dropdown = ''
         self.targeter=''
+
+        self.hidden_col = 5
+        self.layer_nodes_lst = []
+        self.density_matrix = []
+        self.dummy_check_value = tkinter.BooleanVar()
+        self.regression_status = tkinter.BooleanVar()
+        self.dummy_check_status = False
     ###########################################################################
-        self.label = tkinter.Label(root, text="Target", bg="Green", fg='white')
-        self.label.pack()
-        self.label.place(x=20, y=0)
+        # self.label = tkinter.Label(root, text="Target", bg="Green", fg='white')
+        # self.label.pack()
+        # self.label.place(x=20, y=20)
     ############################################################################
-        self.target_box = tkinter.Entry()
-        self.target_box.pack()
-        self.target_box.place(x=70, y=0)
+        
 
     def buildGUI(self, root):
-        self.ImgFrame = Frame(root, width=650, height=500, background="bisque")
-        self.ImgFrame.pack()
-        self.ImgFrame.place(x=20, y=80)
+        # layer_label = tk.Label(root, text="Neural Network Model", width=10, bg='lightblue', anchor='w', pady=8, font=('Verdana', 8, 'bold'))
+        # layer_label.grid(row=layer_row, column=layer_col, sticky='e')
+
+        im = Image.open("images/logo.png")
+        photo = ImageTk.PhotoImage(im)
+
+        label = Label(root, image=photo)
+        label.image = photo  # keep a reference!
+        label.pack()
+        label.place(x=600, y=5)
+
+        self.ModelVizFrame = Frame(root, width=950, height=500, background="bisque")
+        self.ModelVizFrame.pack()
+        self.ModelVizFrame.place(x=40, y=160)
 
         choose_file = Button(root, text="Import data", command=self.chooseFile, height=2, width=18, bg='green', fg='white', font=20)
         choose_file.pack()
-        choose_file.place(x=700, y=150)
+        choose_file.place(x=1140, y=125)
 
-        train = Button(root, text="Train Data", command=self.train_data, height=1, width=9, bg='green', fg='white', font=20)
+        viz_data = Button(root, text="Visualize Data", command=self.viz_data, height=2, width=18, bg='blue', fg='white', font=20)
+        viz_data.pack()
+        viz_data.place(x=1140, y=175)
+
+        train = Button(root, text="Train Data", command=self.train_data, height=2, width=18, bg='green', fg='white', font=20)
         train.pack()
-        train.place(x=700, y=300)
+        train.place(x=1140, y=300)
 
-        test = Button(root, text="Test Data", command=self.train_data, height=1, width=9, bg='green', fg='white', font=20)
-        test.pack()
-        test.place(x=700, y=450)
+        # test = Button(root, text="Test Data", command=self.test_data, height=2, width=18, bg='green', fg='white', font=20)
+        # test.pack()
+        # test.place(x=1000, y=200)
 
-        target_button = Button(root, text="Submit", command=self.retrieve_input, bg='green', fg='white')
-        target_button.pack()
-        target_button.place(x=200, y=0)
+        save_model = Button(root, text="Save Model", command=self.save_model, height=2, width=18, bg='green', fg='white', font=20)
+        save_model.pack()
+        save_model.place(x=1140, y=350)
 
-        # first drop down for Optimizer
-        var = tk.StringVar(root)
-        # initial value
-        var.set('')
-        choices = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta','Adam', 'Adamax', 'Nadam']
-        option = tk.OptionMenu(root, var, *choices)
-        option.pack(side='left', padx=10, pady=10)
-        option.place(x=20, y = 30)
-        button = tk.Button(root, text="Choose optimizer", command=self.dropdown)
-        button.pack(side='left', padx=20, pady=10)
-        button.place(x=130, y = 30)
+
+        reset_btn = Button(root, text="Reset", command=self.reset, height=2, width=18, bg='green', fg='white', font=20)
+        reset_btn.pack()
+        reset_btn.place(x=1140, y=475)
+        # target_button = Button(root, text="Submit", command=self.retrieve_input, bg='green', fg='white')
+        # target_button.pack()
+        # target_button.place(x=200, y=0)
+
+        self.target_box = tkinter.Entry()
+        self.target_box.insert(0, "Target...")
+        self.target_box.pack()
+        self.target_box.place(x=50, y=125)
+
+        self.epoch_box = tkinter.Entry()
+        self.epoch_box.insert(0, "Epochs")
+        self.epoch_box.pack()
+        self.epoch_box.place(x=220, y=125)
+
+        self.batch_box = tkinter.Entry()
+        self.batch_box.insert(0, "Batch size")
+        self.batch_box.pack()
+        self.batch_box.place(x=390, y=125)
+
+        self.dummy_check_box = tkinter.Checkbutton(root, text="Contains Qualitative", variable=self.dummy_check_value, command=self.dummy_handler)
+        self.dummy_check_box.pack()
+        self.dummy_check_box.place(x=560, y=125)
 
         # second drop down menu /// Class Mode
-        var2 = tk.StringVar(root)
+        self.regression_checkbox = tkinter.Checkbutton(root, text="Regression", variable=self.regression_status)
+        self.regression_checkbox.pack()
+        self.regression_checkbox.place(x=730, y=125)
+
+        # first drop down for Optimizer
+        self.var = tk.StringVar(root)
         # initial value
-        var2.set('')
-        choices = ['Binary', 'Multiclass', 'Linear regression']
-        option2 = tk.OptionMenu(root, var2, *choices)
-        option2.pack(side='left', padx=10, pady=10)
-        option2.place(x=300, y = 30)
-        button2 = tk.Button(root, text="Choose Class Mode", command=self.dropdown)
-        button2.pack(side='left', padx=10, pady=10)
-        button2.place(x=370, y = 30)
+        self.var.set('Choose Optimizer')
+        choices = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta','Adam', 'Adamax', 'Nadam']
+        self.option = tk.OptionMenu(root, self.var, *choices)
+        self.option.pack(side='left', padx=10, pady=10)
+        self.option.place(x=860, y = 120)
+        # button = tk.Button(root, text="Choose optimizer", command=self.dropdown)
+        # button.pack(side='left', padx=20, pady=10)
+        # button.place(x=130, y = 30)
 
+        self.input_layer = self.create_layer("Input", 0, 0)
+
+        self.hidden_layer = self.create_layer("Hidden", 0, 2)
+
+        self.add_layer = Button(self.ModelVizFrame, text="+", command=self.create_new_layer, height=1, width=1, bg='green', fg='white')
+        self.add_layer.grid(row=0, column=99)
+
+        self.output_layer = self.create_layer("Output", 0, 100, "Output")
+
+        # button2 = tk.Button(root, text="Choose Class Mode", command=self.dropdown)
+        # button2.pack(side='left', padx=10, pady=10)
+        # button2.place(x=370, y = 30)
+
+    def create_layer(self, layer_label, layer_row, layer_col, layer="Not Output"):
+        layer_label = tk.Label(self.ModelVizFrame, text=layer_label, width=5, bg='lightblue', anchor='w', pady=4, font=('Verdana', 8, 'bold'))
+        layer_label.grid(row=layer_row, column=layer_col, sticky='e')
+
+        im = Image.open("images/layer.png")
+        photo = ImageTk.PhotoImage(im)
+
+        label = Label(self.ModelVizFrame, image=photo)
+        label.image = photo  # keep a reference!
+        label.grid(row=layer_row+1, column=layer_col)  
+
+        if layer != "Output":
+
+            im2 = Image.open("images/weights.png")
+            photo2 = ImageTk.PhotoImage(im2)
+
+            label2 = Label(self.ModelVizFrame, image=photo2)
+            label2.image = photo2  # keep a reference!
+            label2.grid(row=layer_row+1, column=layer_col+1)
+
+            target_box = tkinter.Entry(self.ModelVizFrame, width=4)
+            target_box.insert(0, "10")
+            target_box.grid(row=layer_row+2, column=layer_col)
+
+            self.layer_nodes_lst.append(target_box)
+
+    def create_new_layer(self):
+        if self.hidden_col < 19:
+            layer_label = tk.Label(self.ModelVizFrame, text="Hidden", width=5, bg='lightblue', anchor='w', pady=4, font=('Verdana', 8, 'bold'))
+            layer_label.grid(row=0, column=self.hidden_col, sticky='e')
+
+            im = Image.open("images/layer.png")
+            photo = ImageTk.PhotoImage(im)
+
+            label = Label(self.ModelVizFrame, image=photo)
+            label.image = photo  # keep a reference!
+            label.grid(row=1, column=self.hidden_col)
+
+            im2 = Image.open("images/weights.png")
+            photo2 = ImageTk.PhotoImage(im2)
+
+            label2 = Label(self.ModelVizFrame, image=photo2)
+            label2.image = photo2  # keep a reference!
+            label2.grid(row=1, column=self.hidden_col+1)
+
+            target_box = tkinter.Entry(self.ModelVizFrame, width=4)
+            target_box.insert(0, "10")
+            target_box.grid(row=2, column=self.hidden_col)
+            
+            self.layer_nodes_lst.append(target_box)
+
+            self.hidden_col += 2
+
+    def test_data(self):
+        pass
+        # self.data_ready_test = loader(self.targeter, self.path, self.dummy_check_status,self.regression_status.get(),test_bool=True)
+
+        # tester(self.model,self.data_ready_test)
+        # # self.model.evaluate()
     def train_data(self):
-        self.data_ready = loader(self.targeter, self.path)
-        n_network(self.data_ready)
+        self.density_matrix = []
+        self.data_ready = []
+        self.optimizer_value = self.var.get()
+        # print(self.optimizer_value)
+        # self.class_mode_value = self.var2.get()
+        # print(self.class_mode_value)
 
+        for layer in self.layer_nodes_lst:
+            self.density_matrix.append([int(layer.get())])
+
+        print(self.density_matrix)
+
+        self.targeter = self.target_box.get()
+        self.epochs_size = int(self.epoch_box.get())
+        self.batch_size = int(self.batch_box.get())
+        print(self.targeter)
+        print(self.regression_status.get())
+
+        # self.message_label = tk.Label(root, text="Loading the model. Please, wait...", width=18, bg='lightblue', anchor='w', pady=8)
+        # self.message_label.pack()
+        # self.message_label.place(x=1140, y=85)
+
+        self.data_ready = loader(self.targeter, self.path, self.dummy_check_status,self.regression_status.get())
+        
+        # self.message_label['text'] = "Training the model. Please, wait..."
+        self.run_model()
+        # self.message_label['text'] = "Done Training!"
+        # t1 = threading.Thread(target=self.train_started_message) 
+        # t2 = threading.Thread(target=self.run_model) 
+
+        # starting thread 1 
+        # t1.start() 
+        # starting thread 2 
+        # t2.start() 
+        
+    def run_model(self):
+        self.model = n_network(self.data_ready, self.optimizer_value, self.density_matrix, self.batch_size, self.epochs_size, self.regression_status.get())
+        
+
+    def train_started_message(self):
+        self.message_train_started = tk.messagebox.showinfo("Info", "Training the model. Please, wait...")
+
+    def viz_data(self):
+        robjects.r(r'''
+            library(esquisse)
+
+            data_raw<-read.csv("{0}")
+            esquisser(data_raw)
+            '''.format(self.path))
+
+    def dummy_handler(self):
+        print(self.dummy_check_value.get())
+        if self.dummy_check_value.get() == "off":
+            self.dummy_check_status = False
+            # print(self.dummy_check_status)
+        else:
+            self.dummy_check_status = True
+            # print(self.dummy_check_status)
 
     def chooseFile(self):
         self.file = filer.askopenfilename(initialdir="/", title="Select file", filetypes=(("excel csv files", "*.csv"), ("all files", "*.*")))
         filename = self.file
         print(filename)
         self.path=filename
+    
+    def reset(self):
+        global root
+        root.destroy()
+        main()
 
+    def save_model(self):
+        save(self.model)
 
         # return filename
         # with open(self.file) as file:
         #     file_data = file.read()
         # # print(file_data)
-
-    # we created this function to store the target input but we dont think its working. might not needed but here just in case
-    def retrieve_input(self):
-        self.targeter=self.target_box.get()
-        print(self.targeter)
-        
-
-
-
+ 
 
 
 def main():
--- backend_nn.py
+++ backend_nn.py
@@ -5,62 +5,112 @@ import subprocess
 import pandas as pd
 import rpy2.robjects as robjects
 import numpy as np
+from ann_visualizer.visualize import ann_viz
+from keras.callbacks import History
+import matplotlib.pyplot as plt
+import tkinter as tk
+# from test import test
 
 
 def make_prep():
     subprocess.call(["R the_recepie_scrip_imputedt.R"], shell=True)
-
-def n_network(data,optimizer_var):
+def tester(model,data):
     global num_cat
     multi_class = False
+    # print(x_train[:, 2:])
+    if multi_class == False and not regression_status:
+        y_train = data[:, 1]
+    
+    else:
+        y_train=keras.utils.to_categorical(data[:, 1],num_classes=num_cat) 
+        
+    if regression_status:
+        y_train = y_trainers
+        x_train = data[:, 2:]
+     
+    model.evaluate(x_train,y_train,100,verbose=1)
+
+def n_network(data,optimizer_var, layers, batch_sz, epochs_size, regression_status):
+    global num_cat
+    multi_class = False
+    metrics_value = 'accuracy'
     if num_cat != 1:
         multi_class = True
     # print(x_train[:, 2:])
-    if multi_class == False:
+    if multi_class == False and not regression_status:
         activation_type = 'sigmoid'
         loss_func = 'binary_crossentropy'
         output_layer = 1
         y_train = data[:, 1]
+    
     else:
         activation_type = 'softmax'
         loss_func = 'categorical_crossentropy'
         num_cat += 1
         y_train=keras.utils.to_categorical(data[:, 1],num_classes=num_cat) 
         
-    
+    if regression_status:
+        loss_func='mean_squared_error'
+        y_train = y_trainers
+        metrics_value = 'mae'
+
+    print(type(y_train))
     x_train = data[:, 2:]
     # print(x_train)
     
     # print(y_train)
 
     model=keras.models.Sequential()
-    model.add(keras.layers.Dense(250,activation="relu"))
-    model.add(keras.layers.Dense(250,activation="relu"))
-
-    model.add(keras.layers.Dense(num_cat,activation=activation_type))
+    # model.add(keras.layers.Dense(250,activation="relu"))
+    if len(layers) > 0:
+        for layer in layers:
+            model.add(keras.layers.Dense(layer[0],activation="relu"))
+    # model.add(keras.layers.Dense(250,activation="relu"))
+    if regression_status:
+        model.add(keras.layers.Dense(1))
+    else:
+        model.add(keras.layers.Dense(num_cat,activation=activation_type))
 
 
     model.compile(optimizer=optimizer_var,
                   loss=loss_func,
-                  metrics=["accuracy"],
+                  metrics=[metrics_value],
                   )
-
-    model.fit(x_train,y_train,128,5,validation_split=0.2)
+    hist=History()
+    model.fit(x_train,y_train,batch_sz,epochs_size,validation_split=0.2,callbacks=[hist])
+    # messagebox_status.destroy()
+    messagebox = tk.messagebox.showinfo("Info", "Done Training.")
+    ploter(hist)
+    # ploter(hist["acc"],hist["val_acc"],hist["loss"],hist[val_loss])
+    # ann_viz(model, title="My first neural network")
     return model
+
+def ploter(hist):
+    hist_keys=list(hist.history.keys())
+    print("hist keys",hist_keys)
+    plt.plot(hist.history[hist_keys[1]],'ro-', label=hist_keys[1])
+    plt.plot(hist.history[hist_keys[3]],'go-', label=hist_keys[3])
+    plt.title('Model Accuracy')
+    plt.ylabel(hist_keys[3])
+    plt.xlabel('epoch')
+    plt.legend()
+    plt.show()
 
 def save(model):
     model_str=model.to_json()
-    f_path=Path("model_str.json")
+    f_path=Path("output/model_str.json")
     f_path.write_text(model_str)
-    model.save_weights("model_weights.h5")
+    model.save_weights("output/model_weights.h5")
 
-def loader(target,path):
+def loader(target,path,dummy_input,reg_val,test_bool=False):
 # make_prep()
-
+    tets_switch=""
+    if test_bool:
+        tets_switch="_test"
     # dummy_input = input("Does your data set contain non-numeric values? : ")
     dummy_bool = '#'
-    # if dummy_input == "Y" or dummy_input == "y":
-    #     dummy_bool = ''
+    if dummy_input:
+        dummy_bool = ''
 
     robjects.r(r'''
             library(recipes)
@@ -73,12 +123,19 @@ def loader(target,path):
                 step_scale(all_predictors(),-all_outcomes())%>%
                 prep(data=data_set)
             x_train<-bake(recepie_obj, new_data=data_set)
-            write.csv(as.matrix(x_train),file = "data/data_ready.csv")
-            '''.format(path, target, dummy_bool))
+            write.csv(as.matrix(x_train),file = "data/data_ready{3}.csv")
+            '''.format(path, target, dummy_bool,tets_switch))
 
+    if test_bool:
+        data_ready = pd.read_csv('data/data_ready_test.csv')
+    else:
+        data_ready = pd.read_csv('data/data_ready.csv')
 
-    data_ready = pd.read_csv('data/data_ready.csv')
     output_set = list(set(data_ready[target]))
+    global y_trainers
+    if reg_val==True:
+        data_set=data_ready.values
+        y_trainers=data_set[:,1]
     print(list(set(data_ready[target])))
     global num_cat
     num_cat = len(output_set) - 1
